3fc144a09a1d7454f5ce4234dbe2298a8e546ed7
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-05-05 16:16:44 +0900

    Rubyの重複コードを削除

--- Algorithm1.rb
+++ Algorithm1.rb
@@ -127,31 +127,3 @@ def factorial_2(n)
 
   return n * factorial_2(n-1)
 end
-
-# エラトステネスの篩(ふるい)により最大値maxまでの素数のリストを取得する。
-# maxが整数でない場合、TypeErrorを発生させる。
-def prime_numbers(max)
-  if !max.is_a?(Integer)
-    raise TypeError, "max must be integer"
-  end
-
-  primes = []
-  if max < 0
-    return primes
-  end
-
-  numbers = (2..max).to_a
-  stop_point = Math.sqrt(max).to_i
-  return prime_filter(numbers, primes, stop_point)
-end
-
-def prime_filter(numbers, primes, stop_point)
-  (2..stop_point).each do |n|
-    if numbers[0] == n
-      primes << n
-      numbers = numbers[1..-1].select {|x| x % n != 0}
-    end
-  end
-  primes.concat(numbers)
-  return primes
-end
