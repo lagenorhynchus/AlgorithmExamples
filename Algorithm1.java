0b96630304110e5f85f777be4b695b8bbbac4b8c
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-05-05 16:38:52 +0900

    Java版を新規作成

---
+++ Algorithm1.java
@@ -0,0 +1,88 @@
+/*
+ * 数値計算のアルゴリズム
+ * author: OHASHI Kent
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Algorithm1 {
+    /*
+     * 西暦年yearが閏年かどうかを判定する。
+     */
+    public static boolean isLeapYear(int year) {
+        // TODO: ロジックを実装
+        return false;
+    }
+
+    /*
+     * 整数nが素数かどうかを判定する。
+     */
+    public static boolean isPrime(int n) {
+        // TODO: ロジックを実装
+        return false;
+    }
+
+    /*
+     * ユークリッドの互除法により整数a, bの最大公約数を算出する。
+     * a, bが0以下の場合、nullを返却する。
+     * 除算版
+     */
+    public static Integer gcd(int a, int b) {
+        // TODO: ロジックを実装
+        return null;
+    }
+
+    /*
+     * 減算版
+     */
+    public static Integer gcd2(int a, int b) {
+        // TODO: ロジックを実装
+        return null;
+    }
+
+    /*
+     * 整数a, bの最小公倍数を算出する。
+     * a, bが0以下の場合、nullを返却する。
+     */
+    public static Integer lcm(int a, int b) {
+        // TODO: ロジックを実装
+        return null;
+    }
+
+    /*
+     * フィボナッチ数列の第i項の値を算出する。
+     * iが0以下の場合、nullを返却する。
+     * ループ版
+     */
+    public static Integer fibonacci(int i) {
+        // TODO: ロジックを実装
+        return null;
+    }
+
+    /*
+     * 再帰版
+     */
+    public static Integer fibonacci2(int i) {
+        // TODO: ロジックを実装
+        return null;
+    }
+
+    /*
+     * 整数nの階乗n!の値を算出する。
+     * nが負の数の場合、nullを返却する。
+     * ループ版
+     */
+    public static Integer factorial(int n) {
+        // TODO: ロジックを実装
+        return null;
+    }
+
+    /*
+     * 再帰版
+     */
+    public static Integer factorial2(int n) {
+        // TODO: ロジックを実装
+        return null;
+    }
+}
---
+++ Algorithm2.java
@@ -0,0 +1,50 @@
+/*
+ * リストの探索とソートのアルゴリズム
+ * author: OHASHI Kent
+ */
+
+import java.util.List;
+
+public class Algorithm2 {
+    /*
+     * 逐次探索アルゴリズムによりリストlistの指定した要素targetの位置を取得する。
+     * リスト内に該当する要素がない場合、-1を返却する。
+     */
+    public static int sequentialSearch(List<Integer> list, Integer target) {
+        // TODO: ロジックを実装
+        return -1;
+    }
+
+    /*
+     * 二分探索アルゴリズムによりソート済みリストlistの指定した要素targetの位置を取得する。
+     * リスト内に該当する要素がない場合、-1を返却する。
+     */
+    public static int binarySearch(List<Integer> list, Integer target) {
+        // TODO: ロジックを実装
+        return -1;
+    }
+
+    /*
+     * バブルソートアルゴリズムによりリストlistを昇順にソートする。
+     */
+    public static List<Integer> bubbleSort(List<Integer> list) {
+        // TODO: ロジックを実装
+        return  list;
+    }
+
+    /*
+     * セレクションソートアルゴリズムによりリストlistを昇順にソートする。
+     */
+    public static List<Integer> selectionSort(List<Integer> list) {
+        // TODO: ロジックを実装
+        return list;
+    }
+
+    /*
+     * クイックソートアルゴリズムによりリストlistを昇順にソートする。
+     */
+    public static List<Integer> quickSort(List<Integer> list) {
+        // TODO: ロジックを実装
+        return list;
+    }
+}
