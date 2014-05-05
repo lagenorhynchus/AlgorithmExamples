49a0c28a33aef8dcba9aa58bcb20617ee8b84811
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-05-05 19:31:53 +0900

    JavaScript版を新規作成

---
+++ AlgorithmInJavaScript1.js
@@ -0,0 +1,63 @@
+// 数値計算のアルゴリズム
+// author: OHASHI Kent
+
+// 西暦年yearが閏年かどうかを判定する。
+var isLeapYear = function (year) {
+    // TODO: ロジックを実装
+    return false;
+};
+
+// 整数nが素数かどうかを判定する。
+var isPrime = function (n) {
+    // TODO: ロジックを実装
+    return false;
+};
+
+// ユークリッドの互除法により整数a, bの最大公約数を算出する。
+// a, bが0以下の場合、nullを返却する。
+// 除算版
+var gcd = function (a, b) {
+    // TODO: ロジックを実装
+    return null;
+};
+
+// 減算版
+var gcd2 = function (a, b) {
+    // TODO: ロジックを実装
+    return null;
+};
+
+// 整数a, bの最小公倍数を算出する。
+// a, bが0以下の場合、nullを返却する。
+var lcm = function (a, b) {
+    // TODO: ロジックを実装
+    return null;
+};
+
+// フィボナッチ数列の第i項の値を算出する。
+// iが0以下の場合、nullを返却する。
+// ループ版
+var fibonacci = function (i) {
+    // TODO: ロジックを実装
+    return null;
+};
+
+// 再帰版
+var fibonacci2 = function (i) {
+    // TODO: ロジックを実装
+    return null;
+};
+
+// 整数nの階乗n!の値を算出する。
+// nが負の数の場合、nullを返却する。
+// ループ版
+var factorial = function (n) {
+    // TODO: ロジックを実装
+    return null;
+};
+
+// 再帰版
+var factorial2 = function (n) {
+    // TODO: ロジックを実装
+    return null;
+};
---
+++ AlgorithmInJavaScript2.js
@@ -0,0 +1,34 @@
+// リストの探索とソートのアルゴリズム
+// author: OHASHI Kent
+
+// 逐次探索アルゴリズムによりリストlistの指定した要素targetの位置を取得する。
+// リスト内に該当する要素がない場合、-1を返却する。
+var sequentialSearch = function (list, target) {
+    // TODO: ロジックを実装
+    return -1;
+};
+
+// 二分探索アルゴリズムによりソート済みリストlistの指定した要素targetの位置を取得する。
+// リスト内に該当する要素がない場合、-1を返却する。
+var binarySearch = function (list, target) {
+    // TODO: ロジックを実装
+    return -1;
+};
+
+// バブルソートアルゴリズムによりリストlistを昇順にソートする。
+var bubbleSort = function (list) {
+    // TODO: ロジックを実装
+    return list;
+};
+
+// セレクションソートアルゴリズムによりリストlistを昇順にソートする。
+var selectionSort = function (list) {
+    // TODO: ロジックを実装
+    return list;
+};
+
+// クイックソートアルゴリズムによりリストlistを昇順にソートする。
+var quickSort = function (list) {
+    // TODO: ロジックを実装
+    return list;
+};
