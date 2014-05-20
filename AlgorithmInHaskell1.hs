23d649841a2553df1b5446c30996a4430089a227
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-05-20 22:32:59 +0900

    インデントスタイルを変更

--- AlgorithmInHaskell1.hs
+++ AlgorithmInHaskell1.hs
@@ -17,7 +17,7 @@ isPrime :: Int -> Bool
 isPrime n
     | n < 2     = False
     | otherwise = all (\x -> n `mod` x /= 0) [2..stopPoint]
-        where stopPoint = floor . sqrt $ (fromIntegral n :: Double)
+    where stopPoint = floor . sqrt $ (fromIntegral n :: Double)
 
 -- ユークリッドの互除法により整数a, bの最大公約数を算出する。
 -- a, bが0以下の場合、Nothingを返却する。
@@ -26,19 +26,19 @@ gcd' :: Int -> Int -> Maybe Int
 gcd' a b
     | a < 1 || b < 1 = Nothing
     | otherwise      = divide a b
-        where divide x y
-                  | y <= 0    = Just x
-                  | otherwise = divide y (x `mod` y)
+    where divide x y
+              | y <= 0    = Just x
+              | otherwise = divide y (x `mod` y)
 
 -- 減算版
 gcd2 :: Int -> Int -> Maybe Int
 gcd2 a b
     | a < 1 || b < 1 = Nothing
     | otherwise      = sub a b
-        where sub x y
-                  | x > y     = sub (x - y) y
-                  | x < y     = sub x (y - x)
-                  | otherwise = Just x
+    where sub x y
+              | x > y     = sub (x - y) y
+              | x < y     = sub x (y - x)
+              | otherwise = Just x
 
 -- 整数a, bの最小公倍数を算出する。
 -- a, bが0以下の場合、Nothingを返却する。
@@ -54,17 +54,17 @@ fibonacci :: Int -> Maybe Integer
 fibonacci i
     | i < 1     = Nothing
     | otherwise = Just $ fib i
-        where fib 1 = 1
-              fib 2 = 1
-              fib x = fib (x - 2) + fib (x - 1)
+    where fib 1 = 1
+          fib 2 = 1
+          fib x = fib (x - 2) + fib (x - 1)
 
 -- 末尾再帰版
 fibonacci2 :: Int -> Maybe Integer
 fibonacci2 i
     | i < 1     = Nothing
     | otherwise = Just $ fib i 1 0
-        where fib 1 prev curr = prev + curr
-              fib x prev curr = fib (x - 1) curr (prev + curr)
+    where fib 1 prev curr = prev + curr
+          fib x prev curr = fib (x - 1) curr (prev + curr)
 
 -- ループ版
 fibonacci3 :: Int -> Maybe Integer
@@ -76,9 +76,9 @@ fibonacci4 :: Int -> Maybe Integer
 fibonacci4 i
     | i < 1     = Nothing
     | otherwise = Just $ map fib [(0 :: Int)..] !! i
-        where fib 1 = 1
-              fib 2 = 1
-              fib x = fib (x - 2) + fib (x - 1)
+    where fib 1 = 1
+          fib 2 = 1
+          fib x = fib (x - 2) + fib (x - 1)
 
 -- 整数nの階乗n!の値を算出する。
 -- nが負の数の場合、Nothingを返却する。
@@ -87,18 +87,18 @@ factorial :: Integer -> Maybe Integer
 factorial n
     | n < 0     = Nothing
     | otherwise = Just $ fact n
-        where fact 0 = 1
-              fact 1 = 1
-              fact x = x * fact (x - 1)
+    where fact 0 = 1
+          fact 1 = 1
+          fact x = x * fact (x - 1)
 
 -- 末尾再帰版
 factorial2 :: Integer -> Maybe Integer
 factorial2 n
     | n < 0     = Nothing
     | otherwise = Just $ fact n 1
-        where fact 0 prod = prod
-              fact 1 prod = prod
-              fact x prod = fact (x - 1) (x * prod)
+    where fact 0 prod = prod
+          fact 1 prod = prod
+          fact x prod = fact (x - 1) (x * prod)
 
 -- ループ版
 factorial3 :: Integer -> Maybe Integer
