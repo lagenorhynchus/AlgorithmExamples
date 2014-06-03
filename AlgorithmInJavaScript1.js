5d7dce80f242d692bb4ffe1df9892660ecfe3501
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-06-03 22:35:53 +0900

    停止点計算のバグを修正

--- AlgorithmInJavaScript1.js
+++ AlgorithmInJavaScript1.js
@@ -3,49 +3,109 @@
 
 // 西暦年yearが閏年かどうかを判定する。
 var isLeapYear = function (year) {
-    // TODO: ロジックを実装
-    return false;
+    if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    } else if (year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // 整数nが素数かどうかを判定する。
 var isPrime = function (n) {
-    // TODO: ロジックを実装
-    return false;
+    var stopPoint = 0;
+    var i = 0;
+
+    if (n < 2) {
+        return false;
+    }
+
+    stopPoint = Math.floor(Math.sqrt(n));
+    for (i = 2; i <= stopPoint; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // ユークリッドの互除法により整数a, bの最大公約数を算出する。
 // a, bが1未満の場合、nullを返却する。
 // 除算版
 var gcd = function (a, b) {
-    // TODO: ロジックを実装
-    return null;
+    var r = 0;
+
+    if (a < 1 || b < 1) {
+        return null;
+    }
+
+    while (b > 0) {
+        r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
 };
 
 // 減算版
 var gcd2 = function (a, b) {
-    // TODO: ロジックを実装
-    return null;
+    if (a < 1 || b < 1) {
+        return null;
+    }
+
+    while (true) {
+        if (a > b) {
+            a -= b;
+        } else if (a < b) {
+            b -= a;
+        } else {
+            return a;
+        }
+    }
 };
 
 // 整数a, bの最小公倍数を算出する。
 // a, bが1未満の場合、nullを返却する。
 var lcm = function (a, b) {
-    // TODO: ロジックを実装
-    return null;
+    if (a < 1 || b < 1) {
+        return null;
+    }
+
+    return a * b / gcd(a, b);
 };
 
 // フィボナッチ数列の第i項の値を算出する。
 // iが1未満の場合、nullを返却する。
 // ループ版
 var fibonacci = function (i) {
-    // TODO: ロジックを実装
-    return null;
+    var previous = 1;
+    var current = 0;
+    var j = 0;
+    var tmp = 0;
+
+    if (i < 1) {
+        return null;
+    }
+
+    for (j = 0; j < i; j++) {
+        tmp = previous;
+        previous = current;
+        current = tmp + current;
+    }
+    return current;
 };
 
 // 再帰版
 var fibonacci2 = function (i) {
-    // TODO: ロジックを実装
-    return null;
+    if (i < 1) {
+        return null;
+    } else if (i === 1 || i === 2) {
+        return 1;
+    }
+
+    return fibonacci2(i - 2) + fibonacci2(i - 1);
 };
 
 // 末尾再帰版
@@ -64,14 +124,28 @@ var fibonacci4 = function (i) {
 // nが負の数の場合、nullを返却する。
 // ループ版
 var factorial = function (n) {
-    // TODO: ロジックを実装
-    return null;
+    var product = 1;
+    var i = 0;
+
+    if (n < 0) {
+        return null;
+    }
+
+    for (i = 2; i <= n; i++) {
+        product *= i;
+    }
+    return product;
 };
 
 // 再帰版
 var factorial2 = function (n) {
-    // TODO: ロジックを実装
-    return null;
+    if (n < 0) {
+        return null;
+    } else if (n === 0 || n === 1) {
+        return 1;
+    }
+
+    return n * factorial2(n - 1);
 };
 
 // 末尾再帰版
