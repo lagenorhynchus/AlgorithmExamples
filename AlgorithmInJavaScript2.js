7714e65c3f767364956f41771085e86dc8ce18a2
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-06-14 01:55:23 +0900

    逐次探索,二分探索のロジックを実装

--- AlgorithmInJavaScript2.js
+++ AlgorithmInJavaScript2.js
@@ -4,14 +4,36 @@
 // 逐次探索アルゴリズムによりリストlistの指定した要素targetの位置を取得する。
 // リスト内に該当する要素がない場合、-1を返却する。
 var sequentialSearch = function (list, target) {
-    // TODO: ロジックを実装
+    var i = 0;
+    var len = 0;
+
+    for (i = 0, len = list.length; i < len; i += 1) {
+        if (list[i] === target) {
+            return i;
+        }
+    }
+
     return -1;
 };
 
 // 二分探索アルゴリズムによりソート済みリストlistの指定した要素targetの位置を取得する。
 // リスト内に該当する要素がない場合、-1を返却する。
 var binarySearch = function (list, target) {
-    // TODO: ロジックを実装
+    var low = 0;
+    var high = list.length - 1;
+    var middle = 0;
+
+    while (low <= high) {
+        middle = (low + high) / 2;
+        if (list[middle] > target) {
+            high = middle - 1;
+        } else if (list[middle] < target) {
+            low = middle + 1;
+        } else {
+            return middle;
+        }
+    }
+
     return -1;
 };
 
@@ -31,4 +53,11 @@ var selectionSort = function (list) {
 var quickSort = function (list) {
     // TODO: ロジックを実装
     return list;
+};
+
+var swap = function (list, x, y) {
+    var tmp = list[x];
+
+    list[x] = list[y];
+    list[y] = tmp;
 };
--- AlgorithmInPython2.py
+++ AlgorithmInPython2.py
@@ -3,36 +3,56 @@
 # リストの探索とソートのアルゴリズム
 # author: OHASHI Kent
 
+
 # 逐次探索アルゴリズムによりリストlstの指定した要素targetの位置を取得する。
 # リスト内に該当する要素がない場合、Noneを返却する。
 def sequential_search(lst, target):
-    # TODO: ロジックを実装
-    pass
+    for i, e in enumerate(lst):
+        if e == target:
+            return i
+
+    return None
+
 
 # 二分探索アルゴリズムによりソート済みリストlstの指定した要素targetの位置を取得する。
 # リスト内に該当する要素がない場合、Noneを返却する。
 def binary_search(lst, target):
-    # TODO: ロジックを実装
-    pass
+    low = 0
+    high = len(lst) - 1
+    while low <= high:
+        middle = (low + high) // 2
+        if lst[middle] > target:
+            high = middle - 1
+        elif lst[middle] < target:
+            low = middle + 1
+        else:
+            return middle
+
+    return None
+
 
 # バブルソートアルゴリズムによりリストlstを昇順にソートする。
 def bubble_sort(lst):
     # TODO: ロジックを実装
     pass
 
+
 # セレクションソートアルゴリズムによりリストlstを昇順にソートする。
 def selection_sort(lst):
     # TODO: ロジックを実装
     pass
+
 
 # クイックソートアルゴリズムによりリストlstを昇順にソートする。
 def quick_sort(lst):
     # TODO: ロジックを実装
     pass
 
+
 def q_sort(lst, left, right):
     # TODO: ロジックを実装
     pass
+
 
 def swap(lst, x, y):
     lst[x], lst[y] = lst[y], lst[x]
