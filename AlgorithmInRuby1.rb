7806507cf88d28d4449994e3182d760c2a9dd7cc
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-08-17 18:58:22 +0900

    モジュール化

--- AlgorithmInRuby1.rb
+++ AlgorithmInRuby1.rb
@@ -5,202 +5,206 @@
 # author: OHASHI Kent
 #
 
-# 西暦年yearが閏年かどうかを判定する。
-def leap_year?(year)
-  if year % 4 == 0 && year % 100 != 0
-    return true
-  elsif year % 400 == 0
-    return true
-  else
-    return false
-  end
-end
+module AlgorithmInRuby1
+  module_function
 
-# 整数nが素数かどうかを判定する。
-# ループ版
-def prime?(n)
-  if n < 2
-    return false
-  end
-
-  stop_point = Math.sqrt(n).to_i
-  (2..stop_point).each do |i|
-    if n % i == 0
+  # 西暦年yearが閏年かどうかを判定する。
+  def leap_year?(year)
+    if year % 4 == 0 && year % 100 != 0
+      return true
+    elsif year % 400 == 0
+      return true
+    else
       return false
     end
   end
 
-  return true
-end
-
-# 高階関数版
-def prime_2?(n)
-  if n < 2
-    return false
-  end
-
-  stop_point = Math.sqrt(n).to_i
-  return (2..stop_point).all? {|i| n % i != 0}
-end
-
-# ユークリッドの互除法により整数a, bの最大公約数を算出する。
-# a, bが1未満の場合、nilを返却する。
-# 除算版
-def gcd(a, b)
-  if a < 1 || b < 1
-    return nil
-  end
-
-  while b > 0
-    a, b = b, (a % b)
-  end
-  return a
-end
-
-# 減算版
-def gcd_2(a, b)
-  if a < 1 || b < 1
-    return nil
-  end
-
-  loop do
-    if a > b
-      a -= b
-    elsif a < b
-      b -= a
-    else
-      return a
-    end
-  end
-end
-
-# 整数a, bの最小公倍数を算出する。
-# a, bが1未満の場合、nilを返却する。
-def lcm(a, b)
-  if a < 1 || b < 1
-    return nil
-  end
-
-  return a * b / gcd(a, b)
-end
-
-# フィボナッチ数列の第i項の値を算出する。
-# iが1未満の場合、nilを返却する。
-# ループ版
-def fibonacci(i)
-  if i < 1
-    return nil
-  end
-
-  previous = 1
-  current = 0
-  i.times do
-    previous, current = current, (previous + current)
-  end
-  return current
-end
-
-# 再帰版
-def fibonacci_2(i)
-  if i < 1
-    return nil
-  elsif [1, 2].include?(i)
-    return 1
-  end
-
-  return fibonacci_2(i - 2) + fibonacci_2(i - 1)
-end
-
-# 末尾再帰版
-def fibonacci_3(i)
-  if i < 1
-    return nil
-  end
-
-  def fib(x, previous, current)
-    if x == 1
-      return previous + current
+  # 整数nが素数かどうかを判定する。
+  # ループ版
+  def prime?(n)
+    if n < 2
+      return false
     end
 
-    return fib(x - 1, current, previous + current)
-  end
-  return fib(i, 1, 0)
-end
+    stop_point = Math.sqrt(n).to_i
+    (2..stop_point).each do |i|
+      if n % i == 0
+        return false
+      end
+    end
 
-# 再帰(メモ化)版
-def fibonacci_4(i)
-  if i < 1
-    return nil
+    return true
   end
 
-  def fib(x, fib_hash = {})
-    if [1, 2].include?(x)
+  # 高階関数版
+  def prime_2?(n)
+    if n < 2
+      return false
+    end
+
+    stop_point = Math.sqrt(n).to_i
+    return (2..stop_point).all? {|i| n % i != 0}
+  end
+
+  # ユークリッドの互除法により整数a, bの最大公約数を算出する。
+  # a, bが1未満の場合、nilを返却する。
+  # 除算版
+  def gcd(a, b)
+    if a < 1 || b < 1
+      return nil
+    end
+
+    while b > 0
+      a, b = b, (a % b)
+    end
+    return a
+  end
+
+  # 減算版
+  def gcd_2(a, b)
+    if a < 1 || b < 1
+      return nil
+    end
+
+    loop do
+      if a > b
+        a -= b
+      elsif a < b
+        b -= a
+      else
+        return a
+      end
+    end
+  end
+
+  # 整数a, bの最小公倍数を算出する。
+  # a, bが1未満の場合、nilを返却する。
+  def lcm(a, b)
+    if a < 1 || b < 1
+      return nil
+    end
+
+    return a * b / gcd(a, b)
+  end
+
+  # フィボナッチ数列の第i項の値を算出する。
+  # iが1未満の場合、nilを返却する。
+  # ループ版
+  def fibonacci(i)
+    if i < 1
+      return nil
+    end
+
+    previous = 1
+    current = 0
+    i.times do
+      previous, current = current, (previous + current)
+    end
+    return current
+  end
+
+  # 再帰版
+  def fibonacci_2(i)
+    if i < 1
+      return nil
+    elsif [1, 2].include?(i)
       return 1
-    elsif !fib_hash.key?(x)
-      fib_hash[x] = fib(x - 2) + fib(x - 1)
     end
 
-    return fib_hash[x]
-  end
-  return fib(i)
-end
-
-# 整数nの階乗n!の値を算出する。
-# nが負の数の場合、nilを返却する。
-# ループ版
-def factorial(n)
-  if n < 0
-    return nil
+    return fibonacci_2(i - 2) + fibonacci_2(i - 1)
   end
 
-  product = 1
-  (2..n).each do |i|
-    product *= i
-  end
-  return product
-end
-
-# 再帰版
-def factorial_2(n)
-  if n < 0
-    return nil
-  elsif [0, 1].include?(n)
-    return 1
-  end
-
-  return n * factorial_2(n - 1)
-end
-
-# 末尾再帰版
-def factorial_3(n)
-  if n < 0
-    return nil
-  end
-
-  def fact(x, product)
-    if [0, 1].include?(x)
-      return product
+  # 末尾再帰版
+  def fibonacci_3(i)
+    if i < 1
+      return nil
     end
 
-    return fact(x - 1, product * x)
-  end
-  return fact(n, 1)
-end
+    def fib(x, previous, current)
+      if x == 1
+        return previous + current
+      end
 
-# 畳み込み版
-def factorial_4(n)
-  if n < 0
-    return nil
-  end
-
-  return (2..n).inject(1) {|product, i| product * i}
-end
-
-# 畳み込み版2
-def factorial_5(n)
-  if n < 0
-    return nil
+      return fib(x - 1, current, previous + current)
+    end
+    return fib(i, 1, 0)
   end
 
-  return (2..n).inject(1, :*)
+  # 再帰(メモ化)版
+  def fibonacci_4(i)
+    if i < 1
+      return nil
+    end
+
+    def fib(x, fib_hash = {})
+      if [1, 2].include?(x)
+        return 1
+      elsif !fib_hash.key?(x)
+        fib_hash[x] = fib(x - 2) + fib(x - 1)
+      end
+
+      return fib_hash[x]
+    end
+    return fib(i)
+  end
+
+  # 整数nの階乗n!の値を算出する。
+  # nが負の数の場合、nilを返却する。
+  # ループ版
+  def factorial(n)
+    if n < 0
+      return nil
+    end
+
+    product = 1
+    (2..n).each do |i|
+      product *= i
+    end
+    return product
+  end
+
+  # 再帰版
+  def factorial_2(n)
+    if n < 0
+      return nil
+    elsif [0, 1].include?(n)
+      return 1
+    end
+
+    return n * factorial_2(n - 1)
+  end
+
+  # 末尾再帰版
+  def factorial_3(n)
+    if n < 0
+      return nil
+    end
+
+    def fact(x, product)
+      if [0, 1].include?(x)
+        return product
+      end
+
+      return fact(x - 1, product * x)
+    end
+    return fact(n, 1)
+  end
+
+  # 畳み込み版
+  def factorial_4(n)
+    if n < 0
+      return nil
+    end
+
+    return (2..n).inject(1) {|product, i| product * i}
+  end
+
+  # 畳み込み版2
+  def factorial_5(n)
+    if n < 0
+      return nil
+    end
+
+    return (2..n).inject(1, :*)
+  end
 end
--- AlgorithmInRuby2.rb
+++ AlgorithmInRuby2.rb
@@ -5,95 +5,101 @@
 # author: OHASHI Kent
 #
 
-# 逐次探索アルゴリズムによりリストlistの指定した要素targetの位置を取得する。
-# リスト内に該当する要素がない場合、nilを返却する。
-def sequential_search(list, target)
-  list.each_with_index do |e, i|
-    if e == target
-      return i
-    end
-  end
+module AlgorithmInRuby2
+  module_function
 
-  return nil
-end
-
-# 二分探索アルゴリズムによりソート済みリストlistの指定した要素targetの位置を取得する。
-# リスト内に該当する要素がない場合、nilを返却する。
-def binary_search(list, target)
-  low = 0
-  high = list.length - 1
-  while low <= high
-    middle = (low + high) / 2
-    if list[middle] > target
-      high = middle - 1
-    elsif list[middle] < target
-      low = middle + 1
-    else
-      return middle
-    end
-  end
-
-  return nil
-end
-
-# バブルソートアルゴリズムによりリストlistを昇順にソートする。
-def bubble_sort(list)
-  list = list.dup
-  (0..list.length - 1).each do |i|
-    (0..list.length - i - 2).each do |j|
-      if list[j] > list[j + 1]
-        swap(list, j, j + 1)
+  # 逐次探索アルゴリズムによりリストlistの指定した要素targetの位置を取得する。
+  # リスト内に該当する要素がない場合、nilを返却する。
+  def sequential_search(list, target)
+    list.each_with_index do |e, i|
+      if e == target
+        return i
       end
     end
+
+    return nil
   end
 
-  return list
-end
-
-# セレクションソートアルゴリズムによりリストlistを昇順にソートする。
-def selection_sort(list)
-  list = list.dup
-  (0..list.length - 2).each do |first|
-    min = first
-    (first + 1..list.length - 1).each do |i|
-      if list[i] < list[min]
-        min = i
+  # 二分探索アルゴリズムによりソート済みリストlistの指定した要素targetの位置を取得する。
+  # リスト内に該当する要素がない場合、nilを返却する。
+  def binary_search(list, target)
+    low = 0
+    high = list.length - 1
+    while low <= high
+      middle = (low + high) / 2
+      if list[middle] > target
+        high = middle - 1
+      elsif list[middle] < target
+        low = middle + 1
+      else
+        return middle
       end
     end
-    swap(list, first, min)
+
+    return nil
   end
 
-  return list
-end
+  # バブルソートアルゴリズムによりリストlistを昇順にソートする。
+  def bubble_sort(list)
+    list = list.dup
+    (0..list.length - 1).each do |i|
+      (0..list.length - i - 2).each do |j|
+        if list[j] > list[j + 1]
+          swap(list, j, j + 1)
+        end
+      end
+    end
 
-# クイックソートアルゴリズムによりリストlistを昇順にソートする。
-def quick_sort(list)
-  list = list.dup
-  return q_sort(list, 0, list.length - 1)
-end
-
-def q_sort(list, left, right)
-  if right - left < 1
     return list
   end
 
-  pivot = (left + right) / 2
-  swap(list, left, pivot)
-  last = left
-  (left + 1..right).each do |i|
-    if list[i] < list[left]
-      last += 1
-      swap(list, i, last)
+  # セレクションソートアルゴリズムによりリストlistを昇順にソートする。
+  def selection_sort(list)
+    list = list.dup
+    (0..list.length - 2).each do |first|
+      min = first
+      (first + 1..list.length - 1).each do |i|
+        if list[i] < list[min]
+          min = i
+        end
+      end
+      swap(list, first, min)
     end
+
+    return list
   end
-  swap(list, left, last)
 
-  q_sort(list, left, last - 1)
-  q_sort(list, last + 1, right)
+  # クイックソートアルゴリズムによりリストlistを昇順にソートする。
+  def quick_sort(list)
+    list = list.dup
+    return q_sort(list, 0, list.length - 1)
+  end
 
-  return list
-end
+  def q_sort(list, left, right)
+    if right - left < 1
+      return list
+    end
 
-def swap(list, x, y)
-  list[x], list[y] = list[y], list[x]
+    pivot = (left + right) / 2
+    swap(list, left, pivot)
+    last = left
+    (left + 1..right).each do |i|
+      if list[i] < list[left]
+        last += 1
+        swap(list, i, last)
+      end
+    end
+    swap(list, left, last)
+
+    q_sort(list, left, last - 1)
+    q_sort(list, last + 1, right)
+
+    return list
+  end
+
+  def swap(list, x, y)
+    list[x], list[y] = list[y], list[x]
+  end
+
+  private_class_method :q_sort, :swap
 end
