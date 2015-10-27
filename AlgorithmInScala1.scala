60e41bb3f78c3016bb5e4082093d97751abfdc47
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-10-28 00:10:05 +0900

    implement TODOs

--- AlgorithmInScala1.scala
+++ AlgorithmInScala1.scala
@@ -2,22 +2,26 @@
  * 数値計算のアルゴリズム
  */
 
+import scala.annotation.tailrec
+
 object AlgorithmInScala1 {
   /*
    * 西暦年yearが閏年かどうかを判定する。
    */
-  def isLeapYear(year: Int): Boolean = {
-    // TODO: ロジックを実装
-    false
-  }
+  def isLeapYear(year: Int): Boolean =
+    if (year % 4 == 0 && year % 100 != 0) true
+    else if (year % 400 == 0) true
+    else false
 
   /*
    * 整数nが素数かどうかを判定する。
    */
-  def isPrime(n: Int): Boolean = {
-    // TODO: ロジックを実装
-    false
-  }
+  def isPrime(n: Int): Boolean =
+    if (n < 2) false
+    else {
+      val stopPoint = Math.sqrt(n).toInt
+      (2 to stopPoint).forall(n % _ != 0)
+    }
 
   /*
    * ユークリッドの互除法により整数a, bの最大公約数を算出する。
@@ -25,26 +29,36 @@ object AlgorithmInScala1 {
    * 除算版
    */
   def gcd(a: Int, b: Int): Option[Int] = {
-    // TODO: ロジックを実装
-    None
+    @tailrec
+    def divide(x: Int, y: Int): Int =
+      if (y == 0) x
+      else divide(y, x % y)
+
+    if (a < 1 || b < 1) None
+    else Some(divide(a, b))
   }
 
   /*
    * 減算版
    */
   def gcd2(a: Int, b: Int): Option[Int] = {
-    // TODO: ロジックを実装
-    None
+    @tailrec
+    def subtract(x: Int, y: Int): Int =
+      if (x > y) subtract(x - y, y)
+      else if (x < y) subtract(x, y - x)
+      else x
+
+    if (a < 1 || b < 1) None
+    else Some(subtract(a, b))
   }
 
   /*
    * 整数a, bの最小公倍数を算出する。
    * a, bが1未満の場合、Noneを返却する。
    */
-  def lcm(a: Int, b: Int): Option[Int] = {
-    // TODO: ロジックを実装
-    None
-  }
+  def lcm(a: Int, b: Int): Option[Int] =
+    if (a < 1 || b < 1) None
+    else gcd(a, b).map(a * b / _)
 
   /*
    * フィボナッチ数列の第i項の値を算出する。
